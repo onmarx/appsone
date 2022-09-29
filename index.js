38fb95c3f643ac7ca66ef14b0590872a24f1012e
onmarx <63815909+onmarx@users.noreply.github.com>
2022-09-29 13:07:39 -0500

    agregando enter event

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ function desbloquear(){
   }
   password.value = '';
 }
+
+
+password.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    desbloquear();
+  }
+});
